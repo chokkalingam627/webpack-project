5c91b23ab8b01f6f4396ddcfe09d97da01f18667
dio brante <73946750+GloriaBorger999@users.noreply.github.com>
2024-03-22 07:05:35 +0000

    added webpack, styles, templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const loader = require('sass-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname,'src/index.js'),
+    entry: {
+      main: path.resolve(__dirname,'src/index.js')
+    },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: 'main.js'
+      filename: '[name].js'
     },
     module: {
       rules: [
         {
           test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
       ]
-    }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Nodeapp',
+        filename: 'index.html',
+        template: './src/template.html'
+      })
+    ]
   };
