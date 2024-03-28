ab76763b9edca2484999a82679d64d44d91e0cc6
dio brante <73946750+GloriaBorger999@users.noreply.github.com>
2024-03-28 11:51:00 +0000

    Finished project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import quoteGenerator from "./quotes"
 import './styles/styles.scss'
+import quote from './assets/quote.png'
 
-console.log(quoteGenerator())
+const quoteImg = document.getElementById('quoteImg')
+quoteImg.src = quote
+
+const quoteBtn = document.getElementById('quoteBtn')
+quoteBtn.addEventListener('click', quoteGenerator)
+
+quoteGenerator()
--- src/quotes.js
+++ src/quotes.js
@@ -1,5 +1,14 @@
 function quoteGenerator(){
-    return ""
+    const url = "https://api.quotable.io/random";
+    let getQuote = () => {
+        fetch(url)
+        .then((data) => data.json())
+        .then((item) => {
+            document.getElementById("quote").innerText = item.content
+            console.log(item.author)
+        });
+    };
+    getQuote()
 }
 
 export default quoteGenerator
