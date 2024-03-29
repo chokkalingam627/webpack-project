4b81257cd24e27816a5b8cd3f4e5c4858ef1a31f
dio brante <73946750+GloriaBorger999@users.noreply.github.com>
2024-03-29 04:50:27 +0000

    finished

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const loader = require('sass-loader');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 module.exports = {
     mode: 'development',
@@ -9,13 +10,39 @@ module.exports = {
     },
     output: {
       path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js'
+      filename: '[name][contenthash].js',
+      clean: true,
+      assetModuleFilename: '[name][ext]'
+    },
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, 'dist')
+      },
+      port: 3000,
+      open: true,
+      hot: true,
+      compress: true,
+      historyApiFallback: true
     },
     module: {
       rules: [
         {
           test: /\.scss$/,
           use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            } 
+          }
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|svg)$/i,
+          type: 'asset/resource'
         }
       ]
     },
@@ -24,6 +51,7 @@ module.exports = {
         title: 'Nodeapp',
         filename: 'index.html',
         template: './src/template.html'
-      })
+      }),
+      new BundleAnalyzerPlugin()
     ]
   };
